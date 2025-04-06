cfbcec1fc166dd1cfaadc2167827cfae5fc89463
Henrique Gama <hgamaf@gmail.com>
2025-04-06 10:41:32 -0300

    updated pyarrow

--- feature_store/init_hopsworks.py
+++ feature_store/init_hopsworks.py
@@ -1,7 +1,17 @@
 import hopsworks
+import os
+from dotenv import load_dotenv
+
+# Carrega as variáveis de ambiente do arquivo .env
+load_dotenv()
+
+# Obtém a API key do ambiente
+api_key = os.getenv('HOPSWORKS_API_KEY')
 
 # Inicializa a conexão com o Hopsworks
-project = hopsworks.login()
+project = hopsworks.login(
+    api_key_value=api_key
+)
 
 # Obtém o feature store
 fs = project.get_feature_store()
